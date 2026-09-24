6d13e859cd0b51af26604e19e5305bc505b7e019
Nicole Hucke <100988272+nicole-hucke@users.noreply.github.com>
2026-09-24 11:37:14 -0600

    Package model (#3)
    * Ignore build artifacts
    
    * Add pyproject.toml for packaging
    
    * Make a main program
    
    * Clean lint and reformat code
    
    * Put guards on the title and outfile keywords

--- diffusion_model.py
+++ diffusion_model.py
@@ -1,38 +1,55 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def calculate_stable_time_step(grid_spacing, diffusivity):
     return 0.5 * grid_spacing**2 / diffusivity
+
 
 def plot_profile(xvals, yvals, color="r", title=None, outfile=None):
     plt.figure()
     plt.plot(xvals, yvals, color)
-    plt.xlabel("distance") 
+    plt.xlabel("distance")
     plt.ylabel("elevation")
-    plt.title(title)
-    plt.savefig(outfile)
+    if title is not None:
+        plt.title(title)
+    if outfile is not None:
+        plt.savefig(outfile)
 
-D = 100
-Lx = 300
 
-dx = 0.5 # grid spacing
-x = np.arange(start=0, stop=Lx, step=dx) # the one-dimensional grid
-nx = len(x) # length of the grid 
+if __name__ == "__main__":
+    D = 100
+    Lx = 300
 
-z = np.zeros_like(x)
-z_hi = 500.0
-z_lo = 0.0
+    dx = 0.5  # grid spacing
+    x = np.arange(start=0, stop=Lx, step=dx)  # the one-dimensional grid
+    nx = len(x)  # length of the grid
 
-z[x <= Lx/2] = z_hi
-z[x > Lx/2] = z_lo
+    z = np.zeros_like(x)
+    z_hi = 500.0
+    z_lo = 0.0
 
-plot_profile(x, z, "r", title="Initial hillslope profile", outfile="initial_profile.png")
+    z[x <= Lx / 2] = z_hi
+    z[x > Lx / 2] = z_lo
 
-nt = 5000
-dt = calculate_stable_time_step(dx, D)
+    plot_profile(
+        x,
+        z,
+        "r",
+        title="Initial hillslope profile",
+        outfile="initial_profile.png",
+    )
 
-for t in range(0, nt):
-	z[1:-1] += D * dt / dx ** 2 * (z[:-2] - 2*z[1:-1] + z[2:])
+    nt = 5000
+    dt = calculate_stable_time_step(dx, D)
 
-plot_profile(x, z, "b", title="FInal hillslope profile", outfile="final_profile.png")
+    for t in range(0, nt):
+        z[1:-1] += D * dt / dx**2 * (z[:-2] - 2 * z[1:-1] + z[2:])
 
+    plot_profile(
+        x,
+        z,
+        "b",
+        title="FInal hillslope profile",
+        outfile="final_profile.png",
+    )
--- test_diffusion_model.py
+++ test_diffusion_model.py
@@ -2,5 +2,5 @@ from diffusion_model import calculate_stable_time_step
 
 
 def test_time_step_is_float():
-    time_step = calculate_stable_time_step(1,1)
+    time_step = calculate_stable_time_step(1, 1)
     assert isinstance(time_step, float)
